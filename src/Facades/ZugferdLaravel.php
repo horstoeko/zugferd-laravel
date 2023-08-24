fa47c9ada8f037f7d26bc9bed6902aaa48255de5
ruff <ruff@thor>
2023-08-24 07:46:46 +0200

    Added some new facade methods (Read XML/PDR), Merge

--- src/Facades/ZugferdLaravel.php
+++ src/Facades/ZugferdLaravel.php
@@ -5,12 +5,18 @@ namespace horstoeko\zugferdlaravel\Facades;
 use Illuminate\Support\Facades\Facade;
 
 /**
- * @method static createDocumentInMinimumProfile
- * @method static createDocumentInBasicProfile
- * @method static createDocumentInBasicWlProfile
- * @method static createDocumentInEN16931Profile
- * @method static createDocumentInExtendedProfile
- * @method static createDocumentInXRechnungProfile
+ * @method static \horstoeko\zugferd\ZugferdDocumentBuilder createDocumentInMinimumProfile
+ * @method static \horstoeko\zugferd\ZugferdDocumentBuilder createDocumentInBasicProfile
+ * @method static \horstoeko\zugferd\ZugferdDocumentBuilder createDocumentInBasicWlProfile
+ * @method static \horstoeko\zugferd\ZugferdDocumentBuilder createDocumentInEN16931Profile
+ * @method static \horstoeko\zugferd\ZugferdDocumentBuilder createDocumentInExtendedProfile
+ * @method static \horstoeko\zugferd\ZugferdDocumentBuilder createDocumentInXRechnungProfile
+ * @method static \horstoeko\zugferd\ZugferdDocumentReader readXmlFromFile(string $xmlfilename)
+ * @method static \horstoeko\zugferd\ZugferdDocumentReader readXmlFromString(string $xmlcontent)
+ * @method static \horstoeko\zugferd\ZugferdDocumentReader|null readXmlFromPdfFile(string $pdfFilename)
+ * @method static void buildMergedPdfByDocumentBuilder(ZugferdDocumentBuilder $zugferdDocumentBuilder, string $originalPdfDataOrPdfFilename, string $toPdfFilename)
+ * @method static void buildMergedPdfByXmlDataOrXmlFilename(string $xmlDataOrFilename, string $originalPdfDataOrPdfFilename, string $toPdfFilename)
+ * @method static void buildMergedPdf(mixed $xmlContainer, string $originalPdfDataOrPdfFilename, string $toPdfFilename)
  *
  * @see horstoeko\zugferdlaravel\ZugferdLaravel
  */
--- src/ZugferdLaravel.php
+++ src/ZugferdLaravel.php
@@ -3,6 +3,10 @@
 namespace horstoeko\zugferdlaravel;
 
 use horstoeko\zugferd\ZugferdDocumentBuilder;
+use horstoeko\zugferd\ZugferdDocumentPdfBuilder;
+use horstoeko\zugferd\ZugferdDocumentPdfMerger;
+use horstoeko\zugferd\ZugferdDocumentPdfReader;
+use horstoeko\zugferd\ZugferdDocumentReader;
 use horstoeko\zugferd\ZugferdProfiles;
 
 class ZugferdLaravel
@@ -65,5 +69,85 @@ class ZugferdLaravel
     public function createDocumentInXRechnungProfile(): ZugferdDocumentBuilder
     {
         return new ZugferdDocumentBuilder(ZugferdProfiles::PROFILE_XRECHNUNG_2_2);
+    }
+
+    /**
+     * Read the content of a xml file
+     *
+     * @param string $xmlfilename
+     * @return ZugferdDocumentReader
+     * @throws Exception
+     */
+    public function readXmlFromFile(string $xmlfilename): ZugferdDocumentReader
+    {
+        return ZugferdDocumentReader::readAndGuessFromFile($xmlfilename);
+    }
+
+    /**
+     * Read the content of a xml string
+     *
+     * @param string $xmlcontent
+     * @return ZugferdDocumentReader
+     * @throws Exception
+     */
+    public function readXmlFromString(string $xmlcontent): ZugferdDocumentReader
+    {
+        return ZugferdDocumentReader::readAndGuessFromContent($xmlcontent);
+    }
+
+    /**
+     * Read the content of a pdf containing the xml data (as attached file)
+     *
+     * @param string $pdfFilename
+     * @return ZugferdDocumentReader|null
+     */
+    public function readXmlFromPdfFile(string $pdfFilename): ?ZugferdDocumentReader
+    {
+        return ZugferdDocumentPdfReader::readAndGuessFromFile($pdfFilename);
+    }
+
+    /**
+     * Create a merged PDF (XML as attachment) by the output of a ZugferdDocumentBuilder
+     *
+     * @param ZugferdDocumentBuilder $zugferdDocumentBuilder
+     * @param string $originalPdfDataOrPdfFilename
+     * @param string $toPdfFilename
+     * @return void
+     */
+    public function buildMergedPdfByDocumentBuilder(ZugferdDocumentBuilder $zugferdDocumentBuilder, string $originalPdfDataOrPdfFilename, string $toPdfFilename): void
+    {
+        $zugferdDocumentPdfBuilder = new ZugferdDocumentPdfBuilder($zugferdDocumentBuilder, $originalPdfDataOrPdfFilename);
+        $zugferdDocumentPdfBuilder->generateDocument()->saveDocument($toPdfFilename);
+    }
+
+    /**
+     * Create a merged PDF (XML as attachment) by an existing XML filename or XML-Data-String
+     *
+     * @param string $xmlDataOrFilename
+     * @param string $originalPdfDataOrPdfFilename
+     * @param string $toPdfFilename
+     * @return void
+     */
+    public function buildMergedPdfByXmlDataOrXmlFilename(string $xmlDataOrFilename, string $originalPdfDataOrPdfFilename, string $toPdfFilename): void
+    {
+        $zugferdPdfMerger = new ZugferdDocumentPdfMerger($xmlDataOrFilename, $originalPdfDataOrPdfFilename);
+        $zugferdPdfMerger->generateDocument()->saveDocument($toPdfFilename);
+    }
+
+    /**
+     * Create a merged PDF (XML as attachment) by a ZugferdDocumentBuilder or a XML filename or XML-Data-String
+     *
+     * @param \horstoeko\zugferd\ZugferdDocumentBuilder|string $xmlContainer
+     * @param string $originalPdfDataOrPdfFilename
+     * @param string $toPdfFilename
+     * @return void
+     */
+    public function buildMergedPdf($xmlContainer, string $originalPdfDataOrPdfFilename, string $toPdfFilename): void
+    {
+        if ($xmlContainer instanceof ZugferdDocumentBuilder) {
+            $this->buildMergedPdfByDocumentBuilder($xmlContainer, $originalPdfDataOrPdfFilename, $toPdfFilename);
+        } elseif (is_string($xmlContainer)) {
+            $this->buildMergedPdfByXmlDataOrXmlFilename($xmlContainer, $originalPdfDataOrPdfFilename, $toPdfFilename);
+        }
     }
 }
