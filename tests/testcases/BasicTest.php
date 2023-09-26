a0078025cbcd274867d229669c46a33220b70dc5
ruff <ruff@thor>
2023-09-26 05:18:52 +0200

    Added test infrastructure

---
+++ tests/testcases/BasicTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace horstoeko\zugferd-laravel\tests\testcases;
+
+use horstoeko\zugferd-laravel\tests\TestCase;
+
+class BasicTest extends TestCase
+{
+    public function testSoundCheck(): void
+    {
+        $this->assertTrue(true);
+    }
+}
